217819fe57b7af93b16307e145a4a7eda782740c
mangopudding12 <josemcrum@hotmail.nl>
2014-10-25 14:41:58 +0200

    week 3 mini game is working
    only now I have a monster image problem but I you hope :)

--- week_2_baby_mini_whale_/src/ofApp.cpp
+++ week_2_baby_mini_whale_/src/ofApp.cpp
@@ -1,32 +1,5 @@
 #include "ofApp.h"
 
-
-	// enemy 1 
-	float d2;
-	float yenemy;
-	float speed;
-	float xenemy;
-	float color3; 
-
-	// enemy 2 
-	float Speednew;
-	float xenemy2;
-	float yenemy2;
-	float d3;
-	float color2; 
-
-	// radius player
-	float d1;
-	float color; 
-
-	// game rules 
-	bool LostGame; 
-	float Level;
-	float Special; 
-	bool HorrorSpecial; 
-	bool LostGame2;
-	bool hasBeenTouched; 
-
 //--------------------------------------------------------------
 void ofApp::setup(){
     // player 
@@ -47,67 +20,100 @@ void ofApp::setup(){
 	// Game rules
 	Level = 0; 
 	color2 = 100; 
+	hasTouch = false; 
 
+	// picutere 
+	MM.loadImage("MM.png");
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 	
-	// background 
-	ofBackground (200,color2,200);
+		// background 
+	//	ofBackground (200,color2,200);
 
-	// The color change from player
-	  if (ofDist (xenemy,yenemy,mouseX,mouseY) <= d1 + d2 && hasBeenTouched) 
+
+
+	// The mmmm you lose or aaaa you go to next level code line (enemy up to down)
+	  if (ofDist (xenemy,yenemy,mouseX,mouseY) <= d1 + d2) 
      {
-          color = 100; 
-		  LostGame2 = true; 	
-		  HorrorSpecial = false; 
-		  } else {
-          color = 200;   
-		  LostGame2 = false; 
-     }
+		  // This is the you have lose part 
+            color = 100; 
+		    LostGame2 = true; 	
+		    hasTouch = true; 
+	      }
 
-	   if (ofDist (xenemy2,yenemy2,mouseX,mouseY) <= d1 + d3 && hasBeenTouched)
-       {
-		  LostGame = true;  
+		  // This part say you lose game so froze everything 
+		  else if (hasTouch == true) {
+		     LostGame2 = true;
+		  }
+
+          // aaaa you go to next level part :) 
+	      else {
+             color = 200;   
+		     LostGame2 = false; 
+
+		  // If you get level 5 you get a new enemy from right to left 
+		  if ( Level > 5) { 
+			  Speednew = 14; 
+
+          // Speed from enemy become faster when you get level 9 
+		  } else if (Level > 9){
+			  Speednew = 20;
+
+          // When are lower than level 5 you don't you 2 enemy's 
           } else {
-          color = 200;  
-		  LostGame = false; 
-		}
-
-	  // enemy moving 
-	  yenemy+= speed; 
-	  xenemy2+= Speednew;
+			  Speednew = 0; 
+		  }
+      }
 
 
-	  // enemy fall out the screem 
-	  if (yenemy > 600)
-	  {
-		  yenemy = ofRandom (10,100);
-		  Level++;
-		  xenemy = ofRandom (20,570);
-	  }
 
-	  if (xenemy2 > 600)
-	  {
-		  xenemy2 = 0;
-		  yenemy2 = ofRandom (10,570);
-	  }
+       // The mmmm you lose or aaaa you go to next level code line (enemy right to left)
+	   if (ofDist (xenemy2,yenemy2,mouseX,mouseY) <= d1 + d3)
+        {
+			 // This is the you have lose part 
+			   LostGame = true;  
+			   hasTouch = true; 
+			 } 
+	   
+			 // This part say you lose game so froze everything 
+			  else if (hasTouch == true) {
+			    LostGame = true; 
+			  }
 
-	  // lostgame rules
+              // aaaa you go to next level part :) 
+			  else { 
+			    color = 200;  
+			    LostGame = false;
+		 }
 
-	  if (Level > 5)
-	  {
-		  HorrorSpecial = true; 
-	  } else {
-		  HorrorSpecial = false; 
-	  }
 
-	  if (HorrorSpecial){
-		  Speednew = 12;
-	  } else {
-		  Speednew = 0; 
-	  }
+
+		  // enemy moving 
+		    yenemy+= speed; 
+			xenemy2+= Speednew;
+
+
+		  // enemy fall out the screen (enemy up to down)
+		  if (yenemy > 600)
+		  {
+			  yenemy = ofRandom (10,100);
+			  Level++;
+			  xenemy = ofRandom (20,570);
+		  }
+
+
+		  // (enemy right to left) fall out the screen  
+		  if (xenemy2 > 600)
+		  {
+			  xenemy2 = 0;
+			  yenemy2 = ofRandom (10,570);
+		  }
+
+		
+
+			
 	   
 	
 
@@ -116,50 +122,51 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 	
-	// player 
-	ofSetColor (90,color,color);
-	ofCircle (mouseX,mouseY,d1);
+		// player 
+		ofSetColor (90,color,color);
+		ofCircle (mouseX,mouseY,d1);
 
-	// enemy 
-	ofSetColor (color2,0,color3); 
-	ofCircle (xenemy,yenemy,d2); 
-	ofSetColor (80,color3,color2);
-	ofCircle (xenemy2,yenemy2,d3); 
 
-	// froze the game 
+		// (enemy up down) 
+		ofSetColor (color2,0,color3); 
+		ofCircle (xenemy,yenemy,d2);
 
-	if (hasBeenTouched){
-		
-	} else { 
+		// (enemy right left)
+		ofSetColor (80,color3,color2);
+		ofCircle (xenemy2,yenemy2,d3); 
+
+	
+		// game rules level 
+		ofSetColor(0); 
+		ofDrawBitmapString("Level: " + ofToString(Level), 20, 20); 
+
+
+	if (LostGame)
+	{
+		Speednew = 0; 
+		speed = 0; 
+		color = 100;
+		ofSetColor(0); 
+		ofDrawBitmapString("Your are a peanut \nYour final Level: " + ofToString(Level) +
+			               "\nPress mouse to restart!", 50, 200);
+	    } else {
+			speed = 15;
 	}
 
 
-
-
-
-	// game rules level 
-	ofSetColor(0); 
-    ofDrawBitmapString("Level: " + ofToString(Level), 20, 20); 
-
-	if (LostGame){
-		Speednew = 0; 
-		speed = 0; 
-		color = 100;
-		ofSetColor(0); 
-		ofDrawBitmapString("Your are a peanut \nYour final Level: " + ofToString(Level) +		 
-			                 "\nPress mouse to restart!", 50, 200);
-	    } else {
-			speed = 15;
-	    }
-
-	if (LostGame2){
+	if (LostGame2)
+	{
 		Speednew = 0; 
 		color = 100;
 		speed = 0; 
 		ofSetColor(0); 
-		ofDrawBitmapString("Your are a peanut \nYour final Level: " + ofToString(Level) +		 
-			                 "\nPress mouse to restart!", 50, 200);
-	    } 
+		ofDrawBitmapString("Your are a peanut \nYour final Level: " + ofToString(Level) +						   "\nPress mouse to restart!", 50, 200);
+	} 
+	      // I have a picture problem oooo noooo help me it is a monster really 
+	 //if (Level > 13) {
+		   MM.draw(300,300,400,400);
+		//   }
+
 }
 
 //--------------------------------------------------------------
@@ -169,9 +176,9 @@ void ofApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-	// radius - size enemy 
-	d2 = ofRandom (10,70);
-	xenemy-= ofRandom (0,50); 
+	// both enemy size and color change 
+	d2 = ofRandom (10,50);
+	d3 = ofRandom (10,50); 
 	color2 = ofRandom (30,210); 
 	color3 = ofRandom (100,250); 
 }
@@ -189,24 +196,54 @@ void ofApp::mouseDragged(int x, int y, int button){
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
 // De game starts again if you press the mouse 
-	     // enemy2 
-		 if (LostGame){
-	      Level = 0; 
-		  xenemy2 = ofRandom (-40,-70);
-		  yenemy2 = ofRandom (200,500);
-		  xenemy = ofRandom (100,500);
-		  yenemy = ofRandom (-20,-70);
-		  color2 = 100; 
+	     
+
+		 if (LostGame)
+		 {
+			 // reset the enemy's location and size 
+			 // (enemy up down)  
+			 xenemy = ofRandom (100,500);
+			 yenemy = ofRandom (-20,-70);
+			 d2 = 30; 
+
+			 // (enemy right left) 
+			  xenemy2 = ofRandom (-30,-70);
+			  yenemy2 = ofRandom (200,500);
+			  d3 = 30;
+		  
+			 // reset level to 0 again 
+			  Level = 0; 
+
+			  // reset the color of the player
+			  color2 = 100;
+
+			 // reset game frozen to game alive 
+			  hasTouch = false; 
 		 } 
 
-		 // enemy 
-		 if (LostGame2){
-	      Level = 0; 
-		  xenemy = ofRandom (100,500);
-		  yenemy = ofRandom (-20,-70);
-		  xenemy2 = ofRandom (-20,-70);
-		  yenemy2 = ofRandom (200,500);
-		  color2 = 100;
+		 
+
+		 if (LostGame2)
+		 {
+			  // reset the enemy's location and size 
+			  // (enemy up down)  
+			  xenemy = ofRandom (100,500);
+			  yenemy = ofRandom (-20,-70);
+			  d2 = 30; 
+
+			  // (enemy right left) 
+			 xenemy2 = ofRandom (-30,-70);
+			 yenemy2 = ofRandom (200,500);
+			  d3 = 30;
+		  
+			  // reset level to 0 again 
+			 Level = 0; 
+
+			 // reset the color of the player
+			  color2 = 100;
+
+			 // reset game frozen to game alive 
+			 hasTouch = false; 
 		 } 
 
 }
--- week_2_baby_mini_whale_/src/ofApp.h
+++ week_2_baby_mini_whale_/src/ofApp.h
@@ -18,5 +18,32 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+		// (enemy up down)
+		float d2;
+		float yenemy;
+		float speed;
+		float xenemy;
+		float color3; 
+
+		// (enemy 2 right left) 
+		float Speednew;
+		float xenemy2;
+		float yenemy2;
+		float d3;
+		float color2; 
+
+		// radius player
+		float d1;
+		float color; 
+
+		// game rules 
+		bool LostGame; 
+		float Level;
+		bool LostGame2;
+		bool hasTouch; 
+
+		// picutere 
+		ofImage MM;
 		
 };
